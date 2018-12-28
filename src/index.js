6bdd20cc0fd28bda42d5b129c7363de64dfb3404
daviskimame <hoolykim@gmail.com>
2018-12-28 09:56:04 +0300

    static react twitter app

--- src/index.js
+++ src/index.js
@@ -1,12 +1,89 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//  Twitter component
+function Tweet(){
+    return (
+        <div className="tweet">
+        <Avatar/>
+        <div className="content">
+        <div>
+             <NameWithHandle name="Kimamedavis" handle="@kimdaiv"/>
+             <Time/>
+             </div>
+            <Message/>
+            <div className="buttons">
+                <ReplyButton/>
+                <RetweetButton/>
+                <LikeButton/>
+                <MoreOptionsButton/>
+            </div>
+        </div>
+        </div>
+    );
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// avatar component
+function Avatar(){
+    return(
+        <img
+        src="https://www.gravatar.com/avatar/nothing"
+        className="avatar"
+         alt="avatar" />
+    );
+}
+
+// message component
+function Message(){
+    return(
+        <div className="message">
+        This is why i love code.
+        </div>
+    );
+}
+
+// @Handle component
+// we are using props to define the properties of our Handle object
+function NameWithHandle(props){
+    return (
+        <span className="name-with-handle">
+        <span className="name"> {props.name} </span>
+        <span className="handle">{props.handle}</span>
+        </span>
+    );
+}
+
+// time component
+// this time we are improving and using arrow functions to define time
+const Time = () => (
+  <span className="time">
+    3h ago
+  </span>
+);
+
+// reply button component
+const ReplyButton = () => (
+  <i className="fa fa-reply reply-button"/>
+);
+
+// retweet button component
+const RetweetButton = () => (
+  <i className="fa fa-retweet retweet-button"/>
+);
+
+// like button component
+const LikeButton = () => (
+  <i className="fa fa-heart like-button"/>
+);
+
+// more options button component
+const MoreOptionsButton = () => (
+  <i className="fa fa-ellipsis-h more-options-button"/>
+);
+
+
+ReactDOM.render(
+    <Tweet/>,
+    document.querySelector('#root')
+)
