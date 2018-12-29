c5c9b2c74b346ddd71676469c4c2c1bb2f8a7327
daviskimame <hoolykim@gmail.com>
2018-12-29 17:19:52 +0300

    twitter clone

--- src/index.js
+++ src/index.js
@@ -1,22 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types'
 import './index.css';
 
 //  Twitter component
-function Tweet(){
+// add tweet destructuring
+// Tweet function takes in a tweet object "{tweet}"
+// inside Tweet function iam using Jsx to perform function call to other components
+// remember we are using functions to define our components.
+function Tweet({tweet}){
     return (
         <div className="tweet">
-        <Avatar/>
+        <Avatar hash={tweet.gravatar}/>
         <div className="content">
         <div>
-             <NameWithHandle name="Kimamedavis" handle="@kimdaiv"/>
-             <Time/>
+             <NameWithHandle name = {tweet.author.name} handle ={tweet.author.handle} />
+             <Time timestamp={tweet.timestamp}/>
              </div>
-            <Message/>
+            <Message message={tweet.message}/>
             <div className="buttons">
                 <ReplyButton/>
-                <RetweetButton/>
-                <LikeButton/>
+                <RetweetButton count={tweet.retweets}/>
+                <LikeButton count={tweet.likes}/>
                 <MoreOptionsButton/>
             </div>
         </div>
@@ -24,41 +29,65 @@ function Tweet(){
     );
 }
 
+
+// tweet object
+var testTweet = {
+  message: "This is why i love code.",
+  gravatar: "kim.jpeg",
+  author: {
+    handle: "catperson",
+    name: "IAMA Cat Person"
+  },
+  likes: 2,
+  retweets: 2,
+  timestamp: "2016-07-30 21:24:37"
+};
+
 // avatar component
-function Avatar(){
+function Avatar({hash}){
+    var url = hash;
+    console.log(hash)
     return(
         <img
-        src="https://www.gravatar.com/avatar/nothing"
+        src={url}
         className="avatar"
          alt="avatar" />
     );
 }
 
 // message component
-function Message(){
+// destructing our message function to accept message dynamically
+function Message({message}){
     return(
         <div className="message">
-        This is why i love code.
+        {message}
         </div>
     );
 }
 
 // @Handle component
-// we are using props to define the properties of our Handle object
-function NameWithHandle(props){
+// we are using destructuring to define the properties of our Handle object
+function NameWithHandle({handle, name}){
     return (
         <span className="name-with-handle">
-        <span className="name"> {props.name} </span>
-        <span className="handle">{props.handle}</span>
+        <span className="name"> {name} </span>
+        <span className="handle"> @{handle} </span>
         </span>
     );
 }
 
+NameWithHandle.propTypes = {
+    handle : PropTypes.string.isRequired,
+    name : PropTypes.string.isRequired,
+}
+
+
+
 // time component
 // this time we are improving and using arrow functions to define time
-const Time = () => (
+const Time = ({timestamp}) => (
   <span className="time">
-    3h ago
+    {timestamp}
   </span>
 );
 
@@ -67,23 +96,47 @@ const ReplyButton = () => (
   <i className="fa fa-reply reply-button"/>
 );
 
+// validate retweet to display retweets if count is > 0
+function getRetweetCount(count){
+    if (count > 0)
+        return (
+         <span className="retweet-count">
+        {count}
+         </span>
+        );
+    return null;
+
+}
+
 // retweet button component
-const RetweetButton = () => (
-  <i className="fa fa-retweet retweet-button"/>
+const RetweetButton = ({count}) => (
+    <span className="retweet-count">
+    <i className="fa fa-retweet "/>
+    {getRetweetCount(count)}
+  </span>
 );
 
 // like button component
-const LikeButton = () => (
-  <i className="fa fa-heart like-button"/>
+const LikeButton = ({count}) => (
+    <span className="like-button">
+   <i className="fa fa-heart "/>
+   {count > 0 &&
+      <span className="like-count">
+        {count}
+      </span>}
+   </span>
 );
+
+LikeButton.propTypes = {
+  count: PropTypes.number
+};
 
 // more options button component
 const MoreOptionsButton = () => (
   <i className="fa fa-ellipsis-h more-options-button"/>
 );
 
-
 ReactDOM.render(
-    <Tweet/>,
+    <Tweet tweet={testTweet}/>,
     document.querySelector('#root')
 )
